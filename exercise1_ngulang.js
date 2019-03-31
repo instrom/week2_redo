9ccd86b9c68e65096245ef9ef79284253072a65f
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-03-31 17:23:24 +0700

    exercise1

---
+++ exercise1_ngulang.js
@@ -0,0 +1,44 @@
+// 1. Newton Second Law
+	
+// SET m equals to 600kg
+// SET a equals to 2m
+// SET F as a variable
+
+// CALCULATE F equals to m multiplied by a
+
+// DISPLAY F
+
+// 2. Tahun Kabisat
+
+// SET ‘tahun’ as a variable
+
+// IF tahun mod 4 is equal to 0 AND tahun mod 100 is not equal to 0 
+// 	ELSE IF tahun mod is equal to 0 AND tahun mod 100 is equal to 0 AND tahun mod 400 		is equal to 0
+
+// 	THEN DISPLAY “TAHUN KABISAT”
+// 		ELSE DISPLAY “BUKAN TAHUN KABISAT”
+
+// 3. Laundry Day
+
+// SET ‘pakaian’ equals to any value
+// SET ‘mesin cuci max’ equals to 20
+
+// ADD pakaian by 1
+
+// IF ‘mesin cuci’ is equal to 20
+// 	THEN DISPLAY ‘mesin cuci dinyalakan’
+
+
+// 4. Periksa Kuku
+
+// STORE “kukusiswa” with any value
+// WHILE “kukusiswa” < 40
+// 	ADD “kukusiswa” by 1
+// DISPLAY “kukusiswa”
+
+// STORE “kukupendek” with “ ‘kukusiswa’ mod 2 === 0”
+
+// IF “ ‘kukusiswa’ mod 2 = 0”
+// 	THEN DISPLAY “Pujian dari Guru”
+// ELSE
+// 	DISPLAY “Hukuman dari Guru” 
